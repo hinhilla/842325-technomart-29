d5c6e02f585c9af35441311f1153f5c84f831023
Катя <hinhilla@list.ru>
2020-11-03 16:04:10 +0300

    оживление интерфейса

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 const writeUs = document.querySelector('.modal-write-us-wrapper');
-const openWriteUs = writeUs.querySelector('[name=registration]');
-const closeWriteUs = writeUs.querySelector('.close-modal-write-us');
-const login = writeUs.querySelector('[name=name]');
-const formWriteUs = writeUs.querySelector('.modal-write-us-form');
-const email = writeUs.querySelector('[name=email]');
-const message = writeUs.querySelector('.message');
-const buy = document.querySelector('.button-buy');
+const openWriteUs = document.querySelector('.registration');
+const closeWriteUs = document.querySelector('.close-modal-write-us');
+const login = document.querySelector('.name');
+const buttonWriteUs = document.querySelector('.button-text-us');
+
+
+
+
+const formWriteUs = document.querySelector('.modal-write-us-form');
+const email = document.querySelector('[name=email]');
+const message = document.querySelector('.message');
+const buy = document.querySelectorAll('.button-buy');
 const addInBusket = document.querySelector('.modal-basket-wrapper');
-const closeAddInBusket = addInBusket.querySelector('.close-modal-basket');
+const closeAddInBusket = document.querySelector('.close-modal-basket');
 
 const openMap = document.querySelector('.map');
-const map = addInBusket.querySelector('.map-wrapper');
-const closeMap = addInBusket.querySelector('.close-modal-map');
+const map = document.querySelector('.map-wrapper');
+const closeMap = document.querySelector('.close-modal-map');
 
 
 let ifStorageSupport = true;
@@ -25,10 +30,15 @@ try {
 }  catch (err) {
     ifStorageSupport = false;
   }
+if(openWriteUs) {
+  openWriteUs.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    writeUs.classList.add('show-modal');
+  })
 
-openWriteUs.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  writeUs.classList.add('show-modal');
+};
+
+
 
 
   if (storageLogin) {
@@ -50,18 +60,25 @@ openWriteUs.addEventListener('click', function (evt) {
     else {
     login.focus();
     }
-  }
+  };
+
+buttonWriteUs.addEventListener('click', function (evt){
+evt.preventDefault();
+console.log('1');
+writeUs.classList.add('show-modal');
 });
 
 closeWriteUs.addEventListener('click', function (evt) {
   evt.preventDefault();
+  console.log('2');
   writeUs.classList.remove('show-modal');
   writeUs.classList.remove('error');
 });
 
 formWriteUs.addEventListener('submit', function (evt) {
-  if (!login.value || !email.value) {
+  if (!login.value || !email.value || !message.value) {
     evt.preventDefault();
+
     writeUs.classList.remove('error');
     writeUs.offsetWidth = writeUs.offsetWidth;
     writeUs.classList.add('error');
@@ -77,34 +94,63 @@ formWriteUs.addEventListener('submit', function (evt) {
 });
 
 window.addEventListener('keydown', function (evt) {
-  if (evt.key === 27) {
+  if (evt.keyCode === 27) {
     if (writeUs.classList.contains('show-modal')) {
       evt.preventDefault();
+      console.log('3');
       writeUs.classList.remove('show-modal');
       writeUs.classList.remove('error');
+
     }
   }
 })
-/восхитительно, но не работает/
+buy.forEach((btn) =>{
+  btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    addInBusket.classList.add('show-modal');
+  });
+})
 
-buy.addEventListener('click, function (evt) {
-  evt.preventDefault();
-  addInBusket.classList.add('show-modal');
-});
+if (closeAddInBusket) {
+  closeAddInBusket.addEventListener('click', function (evt) {
 
-closeAddInBusket.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  addInBusket.classList.remove('show-modal');
-});
+    evt.preventDefault();
+    addInBusket.classList.remove('show-modal');
+  });
+
+};
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (buy.classList.contains("show-modal")) {
+    if (addInBusket.classList.contains("show-modal")) {
       evt.preventDefault();
-      buy.classList.remove("show-modal");
+      addInBusket.classList.remove("show-modal");
+
+
     }
   }
 });
 
+
+
+openMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  map.classList.add("show-modal");
+});
+
+closeMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  map.classList.remove("show-modal");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (map.classList.contains("show-modal")) {
+      evt.preventDefault();
+      map.classList.remove("show-modal");
+    }
+  }
+});
 
 
 openMap.addEventListener("click", function (evt) {
