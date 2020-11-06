4614c69ad0264d9c3eedba4b64131a8c662f57e3
Катя <hinhilla@list.ru>
2020-11-06 10:12:04 +0300

    внесение правок

--- js/script.js
+++ js/script.js
@@ -18,151 +18,130 @@ const basket = document.querySelector('.basket');
 const addInBookmarks = document.querySelectorAll('.button-add-bookmarks');
 const container = document.querySelector('.serviсes-list');
 const btns = document.querySelectorAll('.serviсes-item button');
-
 let ifStorageSupport = true;
 let storageLogin = '';
 let storageEmail = '';
-
 try {
-  storageLogin = localStorage.getItem('login');
-  storageEmail = localStorage.getItem('email');
-}  catch (err) {
+    storageLogin = localStorage.getItem('login');
+    storageEmail = localStorage.getItem('email');
+} catch (err) {
     ifStorageSupport = false;
-  };
-
-if(openWriteUs) {
-  openWriteUs.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    writeUs.classList.add('show-modal');
-  })
 };
-
-try {
-buttonWriteUs.addEventListener('click', function (evt){
-evt.preventDefault();
-writeUs.classList.add('show-modal');
-});
-
-closeWriteUs.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  writeUs.classList.remove('show-modal');
-  writeUs.classList.remove('error');
-});
-
-formWriteUs.addEventListener('submit', function (evt) {
-  if (!login.value || !email.value || !message.value) {
-    evt.preventDefault();
-    writeUs.classList.remove('error');
-    writeUs.offsetWidth = writeUs.offsetWidth;
-    writeUs.classList.add('error');
-  }
-  else {
-    if (ifStorageSupport) {
-    localStorage.setItem('login', login.value);
-    localStorage.setItem('email', email.value);
-  }
-  }
-});
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (writeUs.classList.contains('show-modal')) {
-      evt.preventDefault();
-      writeUs.classList.remove('show-modal');
-      writeUs.classList.remove('error');
-    }
-  }
-});
-
-openMap.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  map.classList.add("show-modal");
-});
-
-closeMap.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  map.classList.remove("show-modal");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (map.classList.contains("show-modal")) {
-      evt.preventDefault();
-      map.classList.remove("show-modal");
-    }
-  }
-});
-
-openMap.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  map.classList.add("show-modal");
-});
-
-closeMap.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  map.classList.remove("show-modal");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (map.classList.contains("show-modal")) {
-      evt.preventDefault();
-      map.classList.remove("show-modal");
-    }
-  }
-});
-
-const toggleTab = (pointer) => {
-  tabsContent.forEach((tab) => {
-    tab.classList.remove('current-services');
-    if (tab.classList.contains('${pointer}')) {
-      tab.classList.add('current-services');
-    }
-  })
-  };
-
-container.addEventListener('click', function (evt) {
-  if (evt.target === evt.target.closest('button')) {
-    btns.forEach((btn) => {
-      btn.classList.remove('active-service')
+if (openWriteUs) {
+    openWriteUs.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        writeUs.classList.add('show-modal');
     })
-    evt.target.classList.add('active-service');
-    const pointer = evt.target.dataset.id;
-    toggleTab(pointer)
-  }
-})
-}
-catch (e) {
-  console.log('нет такого окна');
-}
-
-buy.forEach((btn) =>{
-  btn.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    addInBusket.classList.add('show-modal');
-    basket.classList.add('added');
-  });
-});
-
-addInBookmarks.forEach((btn) =>{
-  btn.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    bookmarks.classList.add('added');
-  });
-});
-
-if (closeAddInBusket) {
-  closeAddInBusket.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    addInBusket.classList.remove('show-modal');
-  });
 };
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (addInBusket.classList.contains("show-modal")) {
-      evt.preventDefault();
-      addInBusket.classList.remove("show-modal");
+try {
+    buttonWriteUs.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        writeUs.classList.add('show-modal');
+    });
+    closeWriteUs.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        writeUs.classList.remove('show-modal');
+        writeUs.classList.remove('error');
+    });
+    formWriteUs.addEventListener('submit', function(evt) {
+        if (!login.value || !email.value || !message.value) {
+            evt.preventDefault();
+            writeUs.classList.remove('error');
+            writeUs.offsetWidth = writeUs.offsetWidth;
+            writeUs.classList.add('error');
+        } else {
+            if (ifStorageSupport) {
+                localStorage.setItem('login', login.value);
+                localStorage.setItem('email', email.value);
+            }
+        }
+    });
+    window.addEventListener('keydown', function(evt) {
+        if (evt.keyCode === 27) {
+            if (writeUs.classList.contains('show-modal')) {
+                evt.preventDefault();
+                writeUs.classList.remove('show-modal');
+                writeUs.classList.remove('error');
+            }
+        }
+    });
+    openMap.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        map.classList.add('show-modal');
+    });
+    closeMap.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        map.classList.remove('show-modal');
+    });
+    window.addEventListener('keydown', function(evt) {
+        if (evt.keyCode === 27) {
+            if (map.classList.contains('show-modal')) {
+                evt.preventDefault();
+                map.classList.remove('show-modal');
+            }
+        }
+    });
+    openMap.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        map.classList.add('show-modal');
+    });
+    closeMap.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        map.classList.remove('show-modal');
+    });
+    window.addEventListener('keydown', function(evt) {
+        if (evt.keyCode === 27) {
+            if (map.classList.contains('show-modal')) {
+                evt.preventDefault();
+                map.classList.remove('show-modal');
+            }
+        }
+    });
+    const toggleTab = function(pointer) {
+        tabsContent.forEach( function(tab) {
+            tab.classList.remove('current-services');
+            if (tab.dataset.id === pointer) {
+                tab.classList.add('current-services');
+            }
+        })
+    };
+    container.addEventListener('click', function(evt) {
+        if (evt.target === evt.target.closest('button')) {
+            btns.forEach( function(btn) {
+                btn.classList.remove('active-service')
+            })
+            evt.target.classList.add('active-service');
+            const pointer = evt.target.dataset.id;
+            toggleTab(pointer)
+        }
+    })
+} catch (e) {
+    console.log('нет такого окна');
+}
+buy.forEach( function(btn) {
+    btn.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        addInBusket.classList.add('show-modal');
+        basket.classList.add('added');
+    });
+});
+addInBookmarks.forEach( function(btn) {
+    btn.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        bookmarks.classList.add('added');
+    });
+});
+if (closeAddInBusket) {
+    closeAddInBusket.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        addInBusket.classList.remove('show-modal');
+    });
+};
+window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        if (addInBusket.classList.contains('show-modal')) {
+            evt.preventDefault();
+            addInBusket.classList.remove('show-modal');
+        }
     }
-  }
 });
